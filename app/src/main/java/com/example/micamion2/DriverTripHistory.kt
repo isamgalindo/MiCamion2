a6dc6210e9dcdd0571853745bb6beceb9e4810c6
JuanFerrxr <jcferrere1032@gmail.com>
2023-10-03 23:41:13 -0500

    Admin trucks and company trip history views

---
+++ app/src/main/java/com/example/micamion2/DriverTripHistory.kt
@@ -0,0 +1,9 @@
+package com.example.micamion2
+
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class DriverTripHistory {
+}
