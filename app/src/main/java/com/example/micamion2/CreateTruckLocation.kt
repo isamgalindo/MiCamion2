7a17571ad363f1606f612418a7ee2fda514cbcec
isamgalindo <l.martinezg@uniandes.edu.co>
2023-11-06 18:56:20 -0500

    Actual location Truck

--- app/src/main/java/com/example/micamion2/CreateTruckLocation.kt
+++ app/src/main/java/com/example/micamion2/CreateTruckLocation.kt
@@ -2,12 +2,16 @@ package com.example.micamion2
 
 import android.content.Context
 import android.content.Intent
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.Toast
+import java.io.IOException
+import java.util.Locale
 
 class CreateTruckLocation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,21 +39,70 @@ class CreateTruckLocation : AppCompatActivity() {
             val intent = Intent(this, CreateTruckDate::class.java)
             startActivity(intent)
         }
+
         val searchIcon = findViewById<ImageView>(R.id.searchIcon1)
         val searchIcon2 = findViewById<ImageView>(R.id.searchIcon2)
+        val pickUpEditText = findViewById<EditText>(R.id.pickUpLocation)
+        val dropOffEditText = findViewById<EditText>(R.id.destination)
+
+        fun getAddressFromLocation(latitude: Double, longitude: Double, editText: EditText) {
+            val geocoder = Geocoder(this, Locale.getDefault())
+            try {
+                val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+                addresses?.let {
+                    if (it.isNotEmpty()) {
+                        val address = it[0]
+                        val addressFragments = (0..address.maxAddressLineIndex).map(address::getAddressLine)
+
+                        // Combine the address fragments into a single string
+                        val fullAddress = addressFragments.joinToString(separator = "\n")
+
+                        // Now you have a full address string, you can set it to your EditText
+                        runOnUiThread {
+                            editText.setText(fullAddress)
+                        }
+                    } else {
+                        // Handle case where no address was found.
+                        runOnUiThread {
+                            Toast.makeText(this, "No address found", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            } catch (e: IOException) {
+                // Handle the IO exception
+                runOnUiThread {
+                    Toast.makeText(this, "Geocoder service not available", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
 
         searchIcon.setOnClickListener {
             val intent = Intent(this, FuncAF::class.java)
             startActivity(intent)
-            // Handle the click on the icon here
-            // You can open a search dialog or perform any action when the icon is clicked
+            val sharedPreferences = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+            val latitude = sharedPreferences.getFloat("location_lat", 0f)
+            val longitude = sharedPreferences.getFloat("location_lng", 0f)
+            // Check if we actually have a location saved (i.e., not 0f for both)
+            if (latitude.toDouble() != 0.0 && longitude.toDouble() != 0.0) {
+                getAddressFromLocation(latitude.toDouble(), longitude.toDouble(), pickUpEditText)
+            } else {
+                Toast.makeText(this, "No location saved", Toast.LENGTH_SHORT).show()
+            }
         }
 
         searchIcon2.setOnClickListener {
             val intent = Intent(this, FuncAF::class.java)
             startActivity(intent)
-            // Handle the click on the icon here
-            // You can open a search dialog or perform any action when the icon is clicked
+            val sharedPreferences = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+            val latitude = sharedPreferences.getFloat("location_lat", 0f)
+            val longitude = sharedPreferences.getFloat("location_lng", 0f)
+            // Check if we actually have a location saved (i.e., not 0f for both)
+            if (latitude.toDouble() != 0.0 && longitude.toDouble() != 0.0) {
+                getAddressFromLocation(latitude.toDouble(), longitude.toDouble(), dropOffEditText)
+            } else {
+                Toast.makeText(this, "No location saved", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
