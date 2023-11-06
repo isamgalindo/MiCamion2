d77dd22d3d0a773a878ab9d4558b433fbdddb489
isamgalindo <l.martinezg@uniandes.edu.co>
2023-11-06 17:02:20 -0500

    Show trucks

--- app/src/main/java/com/example/micamion2/Trailer.kt
+++ app/src/main/java/com/example/micamion2/Trailer.kt
@@ -3,27 +3,36 @@ package com.example.micamion2
 import androidx.annotation.StringDef
 
 class Trailer {
-    @StringDef(LOAD_OWNER, DRIVER, TRAILER_OWNER)
+    @StringDef(INTRANSIT, AVAILABLE, UNAVAILABLE)
     annotation class Status
-
+    @StringDef(FLATBED, DRYVAN, REEFER, LOWBOY, STEPDECK, OTHER, ANY)
+    annotation class Type
     companion object {
-        const val LOAD_OWNER = "LO"
-        const val DRIVER = "DR"
-        const val TRAILER_OWNER = "TO"
+        const val AVAILABLE = "AV"
+        const val INTRANSIT = "IT"
+        const val UNAVAILABLE= "UN"
+        const val FLATBED = "FB"
+        const val DRYVAN = "DV"
+        const val REEFER= "RF"
+        const val LOWBOY = "LB"
+        const val STEPDECK = "SD"
+        const val OTHER= "OT"
+        const val ANY= "AN"
     }
-
     var plates: String = ""
     var capacity: Int = 0
     var volume: Int? = null
-    var pickUp: String = ""
-    var dropOff: String = ""
-    var type: String = ""
+    var pickup: String = ""
+    var dropoff: String = ""
     var driver: Int? = null
     var owner: Int? = null
+    @Type
+    var type: String = FLATBED
     @Status
-    var status: String = LOAD_OWNER
+    var status: String = INTRANSIT
+
 
     override fun toString(): String {
-        return "$plates $capacity $volume $pickUp $dropOff $type $driver $owner"
+        return "$plates $capacity $volume $pickup $dropoff $type $driver $owner"
     }
 }
