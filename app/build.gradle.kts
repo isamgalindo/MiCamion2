c4800b1f36963ed122331dce1a9fc0c655396c0a
JuanFerrxr <jcferrere1032@gmail.com>
2023-11-06 19:25:21 -0500

    Implemented lost connectivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,9 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    //noinspection GradleCompatible
+    implementation("com.android.support:design:27.1.1")
+    implementation ("com.squareup.retrofit2:converter-scalars:2.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
