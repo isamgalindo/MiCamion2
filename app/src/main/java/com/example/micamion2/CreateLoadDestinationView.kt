d3e304d51352efd7708fc1535f068c5caadc67ab
isamgalindo <l.martinezg@uniandes.edu.co>
2023-11-20 17:37:58 -0500

    back button destination details load

--- app/src/main/java/com/example/micamion2/CreateLoadDestinationView.kt
+++ app/src/main/java/com/example/micamion2/CreateLoadDestinationView.kt
@@ -13,6 +13,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.io.IOException
 import java.util.Locale
 
@@ -20,6 +21,13 @@ class CreateLoadDestinationView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_load_destination_view)
+
+        val backButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        // Set an OnClickListener on the button
+        backButton.setOnClickListener {
+            // Finish the current activity
+            finish()
+        }
 
         val progressbar = findViewById<ProgressBar>(R.id.progressBar)
         val currentprogress = 50
