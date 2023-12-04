b60eba2a1c0d553f6de0b707ed8a90cbaa13b163
isamgalindo <l.martinezg@uniandes.edu.co>
2023-12-04 14:55:15 -0500

    Arreglos de assigned trailer in driver

--- app/src/main/java/com/example/micamion2/AssignTrailerDriver.kt
+++ app/src/main/java/com/example/micamion2/AssignTrailerDriver.kt
@@ -24,9 +24,16 @@ import java.util.Locale
 class AssignTrailerDriver : AppCompatActivity() {
 
     private val userService = RetrofitInstance.apiUsuario
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_assign_trailer_driver)
+
+        val sharedPref = getSharedPreferences("LoadDetails", Context.MODE_PRIVATE)
+        val id = sharedPref.getInt("userId", 0 ).toString() ?: "DefaultIdFallback"
+
+        // Fetch and update trailer data initially
+        fetchAndUpdateTrailerData(id)
 
         val backButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         // Set an OnClickListener on the button
@@ -34,38 +41,25 @@ class AssignTrailerDriver : AppCompatActivity() {
             // Finish the current activity
             finish()
         }
-
-        val sharedPref = getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
-        val id = sharedPref.getString("id", "DefaultId") ?: "DefaultIdFallback"
-        fetchAndUpdateTrailerData(id)
-
         val buttonOK = findViewById<Button>(R.id.AssignButton)
         buttonOK.setOnClickListener {
-            if (id != null) {
-                val request = AssignDriverRequest(id)
-                userService.assignTrailer(request).enqueue(object : Callback<Void>  {
+            val request = AssignDriverRequest(id)
+            userService.assignTrailer(request).enqueue(object : Callback<Void> {
                 override fun onResponse(call: Call<Void>, response: Response<Void>) {
                     if (response.isSuccessful) {
-                        // The request was successful (status code 200-299)
                         Toast.makeText(applicationContext, "Bien", Toast.LENGTH_SHORT).show()
+                        // Fetch and update trailer data after successful assignment
                         fetchAndUpdateTrailerData(id)
-                        // Perform actions based on successful response
                     } else {
                         val errorBody = response.errorBody()?.string()
-                        // The server responded with a status code that isn't in the range 200-299
                         Toast.makeText(applicationContext, "Error: $errorBody", Toast.LENGTH_SHORT).show()
-                        // Handle the server response error
                     }
-                    // Handle response
                 }
 
                 override fun onFailure(call: Call<Void>, t: Throwable) {
                     Toast.makeText(applicationContext, "Mal", Toast.LENGTH_SHORT).show()
                 }
             })
-            }else{
-                Toast.makeText(applicationContext, "User ID is null", Toast.LENGTH_SHORT).show()
-            }
         }
     }
 
@@ -73,43 +67,34 @@ class AssignTrailerDriver : AppCompatActivity() {
         Thread {
             try {
                 val response = userService.getTrailerByDriver(userId).execute()
-                // Check if the response is successful and has body
                 if (response.isSuccessful && response.body() != null) {
                     val trailers = response.body()!!
 
-                    // Check if the trailers list is not empty
                     if (trailers.isNotEmpty()) {
-                        // Assuming you want to display the first trailer's properties
                         val trailer = trailers.first()
-
                         runOnUiThread {
-                            // Update the TextView on the main thread
                             findViewById<TextView>(R.id.platesTrailer).text = trailer.plates
-                            findViewById<TextView>(R.id.capacityTrailer).text =
-                                trailer.capacity.toString()
+                            findViewById<TextView>(R.id.capacityTrailer).text = trailer.capacity.toString()
                             findViewById<TextView>(R.id.statusTrailer).text = trailer.status
-
                         }
                     } else {
                         runOnUiThread {
-                            findViewById<TextView>(R.id.platesTrailer).text =
-                                "No trailer assign"
+                            findViewById<TextView>(R.id.platesTrailer).text = "No trailer assign"
                         }
                     }
                 } else {
                     runOnUiThread {
-                        findViewById<TextView>(R.id.platesTrailer).text =
-                            "Response not successful"
+                        findViewById<TextView>(R.id.platesTrailer).text = "${response.code()}"
                     }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
                 runOnUiThread {
-                    // Update UI for error on the main thread
                     findViewById<TextView>(R.id.platesTrailer).text = "Request failed"
                 }
             }
         }.start()
     }
 }
+
 
